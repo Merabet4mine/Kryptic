b1960ef1cfb7d5e722903fbaa5c272c7b5347d07
Merabet Amine <65823990+MerabetAmine@users.noreply.github.com>
2020-08-04 15:22:22 +0100

    Update KSignature.kt

--- src/kryptic/signature/utils/KSignature.kt
+++ src/kryptic/signature/utils/KSignature.kt
@@ -11,12 +11,13 @@ abstract class KSignature : Kryptic {
     private var sig_algorithm = ""
     private var key_algorithm = ""
 
-    constructor(sig_algorithm:String,key_algorithm:String){
+    protected constructor(sig_algorithm:String,key_algorithm:String){
         this.sig_algorithm = sig_algorithm
         this.key_algorithm = key_algorithm
     }
 
-    fun sign(data:ByteArray) : Pair<ByteArray, KeyPair>? {
+    
+    fun pairSignKeys(data:ByteArray): Pair<ByteArray, KeyPair>? {
         return try {
             val signature = Signature.getInstance(sig_algorithm)
             val keyPair = KPair(key_algorithm)?.pairKey() ?: return null
@@ -27,39 +28,21 @@ abstract class KSignature : Kryptic {
         }
         catch (e:Exception) { except(e.message, null) }
     }
-
-    fun sign(data:String) : Pair<ByteArray, KeyPair>? {
-        return sign((data.toByteArray()))
+    fun pairSignKeys(data:String): Pair<ByteArray, KeyPair>? {
+        return pairSignKeys((data.toByteArray()))
     }
 
-    fun signToString(data:ByteArray) : Triple<String, String, String>? {
-        val pair = sign(data) ?: return null
+    fun tripleSignKeysString(data:ByteArray): Triple<String,String,String>? {
+        val pair = pairSignKeys(data) ?: return null
         val signature = String64(pair.first)
         val publicKey = String64(pair.second.public.encoded)
         val privateKey = String64(pair.second.private.encoded)
         return Triple(signature, publicKey, privateKey)
     }
-
-    fun signToString(data:String) : Triple<String, String, String>? {
-        return signToString(data.toByteArray())
+    fun tripleSignKeysString(data:String): Triple<String, String, String>? {
+        return tripleSignKeysString(data.toByteArray())
     }
 
-
-    ////////////////////////////////////////////////
-
-    /*
-    fun verify(input :String, public_key :String, charset: Charset = Charsets.UTF_8): Boolean {
-        return verify(input.toByteArray(charset), public_key.toByteArray(charset))
-    }
-
-    fun verify(input :ByteArray, public_key :String, charset: Charset = Charsets.UTF_8): Boolean {
-        return verify(input, public_key.toByteArray(charset))
-    }
-
-    fun verify(input :String, public_key :ByteArray, charset: Charset = Charsets.UTF_8): Boolean {
-        return verify(input.toByteArray(charset), public_key)
-    }
-    */
 
     fun verify(data: ByteArray, signature: ByteArray, public_key: ByteArray): Boolean{
         val sign = Signature.getInstance(sig_algorithm)
